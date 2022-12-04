cc2b54b17e9c71530ed18d5d368468b1902ffb52
Akira Komamura <akira.komamura@gmail.com>
2022-12-04 22:56:26 +0900

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    nixos-generators,
+    ...
+  }: let
+    clientSystem = "x86_64-linux";
+  in {
+    colmena = {
+      meta = {
+        nixpkgs = import nixpkgs {
+          system = clientSystem;
+          overlays = [];
+        };
+      };
+
+      zhuang = {
+        deployment = {
+          # A fixed IP address is configured in the router
+          targetHost = "192.168.0.60";
+          targetPort = 22;
+          targetUser = "root";
+        };
+        nixpkgs.system = "aarch64-linux";
+        imports = [
+          <nixpkgs/nixos/modules/installer/sd-card/sd-image-aarch64.nix>
+          ./config/zhuang/initial.nix
+        ];
+      };
+    };
+
+    packages.${clientSystem} = {
+      # Use nixos-generators to bootstrap
+      zhuang = nixos-generators.nixosGenerate {
+        system = "aarch64-linux";
+        format = "sd-aarch64";
+        modules = [
+          ./config/zhuang/initial.nix
+        ];
+      };
+    };
+  };
+}
