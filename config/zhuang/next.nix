3c9d1159dc7627189a61d19bf3a2a24f7bb08e9e
Akira Komamura <akira.komamura@gmail.com>
2022-12-04 23:35:59 +0900

    Add git and git-annex

--- config/zhuang/next.nix
+++ config/zhuang/next.nix
@@ -3,4 +3,16 @@
   services.tailscale = {
     enable = true;
   };
+
+  programs.git = {
+    enable = true;
+    config = {
+      user.name = "Akira Komamura";
+      user.email = "akira.komamura@gmail.com";
+    };
+  };
+
+  environment.systemPackages = [
+    pkgs.git-annex
+  ];
 }
