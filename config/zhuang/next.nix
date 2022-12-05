30cb6a5732ff18273f6583f9fb1e7e9ded4b8e2b
Akira Komamura <akira.komamura@gmail.com>
2022-12-05 09:34:51 +0900

    Add AdGuard Home

--- config/zhuang/next.nix
+++ config/zhuang/next.nix
@@ -5,13 +5,34 @@
     allowedTCPPorts = [
       # ssh
       22
+      # adguardhome
+      53
       # tailscale
       41641
+    ];
+    allowedUDPPorts = [
+      53
     ];
   };
 
   services.tailscale = {
     enable = true;
+  };
+
+  services.adguardhome = {
+    enable = true;
+    openFirewall = true;
+    settings = {
+      bind_port = 3000;
+      bind_host = "0.0.0.0";
+      dns = {
+        bind_port = 3000;
+        bind_host = "0.0.0.0";
+        bootstrap_dns = [
+          "1.1.1.1"
+        ];
+      };
+    };
   };
 
   programs.git = {
