c41f87efb072850622d200d710f9b5958de62727
Akira Komamura <akira.komamura@gmail.com>
2022-12-14 16:06:03 +0900

    Add nix-on-droid configuration

--- flake.nix
+++ flake.nix
@@ -2,6 +2,15 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
     unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-22.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-on-droid = {
+      url = "github:t184256/nix-on-droid/release-22.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
     nixos-generators = {
       url = "github:nix-community/nixos-generators";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -12,11 +21,36 @@
     nixpkgs,
     unstable,
     nixos-generators,
+    nix-on-droid,
+    home-manager,
     ...
   }: let
     clientSystem = "x86_64-linux";
 
     pkgs = nixpkgs.legacyPackages.${clientSystem};
+
+    droidPkgs = import nixpkgs {
+      system = "aarch64-linux";
+      overlays = [
+        nix-on-droid.overlays.default
+      ];
+    };
+
+    mkDroidConfiguration = {modules}:
+      nix-on-droid.lib.nixOnDroidConfiguration {
+        pkgs = droidPkgs;
+        home-manager-path = home-manager.outPath;
+        # extraSpecialArgs = { };
+        modules =
+          [
+            {
+              system.stateVersion = "22.11";
+              # Minimize the number of nixpkgs instance
+              home-manager.useGlobalPkgs = true;
+            }
+          ]
+          ++ modules;
+      };
   in {
     colmena = {
       meta = {
@@ -38,6 +72,20 @@
           <nixpkgs/nixos/modules/installer/sd-card/sd-image-aarch64.nix>
           ./config/zhuang/initial.nix
           ./config/zhuang/rest.nix
+        ];
+      };
+    };
+
+    nixOnDroidConfigurations = {
+      default = mkDroidConfiguration {
+        modules = [
+          {
+            environment.etcBackupExtension = ".bak";
+            environment.packages = with pkgs; [
+              emacs
+              git
+            ];
+          }
         ];
       };
     };
