693ef99065bfdb408224b9d5319b39474934e226
Akira Komamura <akira.komamura@gmail.com>
2022-12-23 22:52:45 +0900

    Adopt flake-parts to organize flake.nix

--- flake.nix
+++ flake.nix
@@ -4,63 +4,77 @@
     nixpkgs.url = "stable";
     unstable.url = "unstable";
 
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
     nixos-generators = {
       url = "github:nix-community/nixos-generators";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
   outputs = {
-    self,
     nixpkgs,
     unstable,
+    flake-parts,
     nixos-generators,
     ...
-  }: let
-    clientSystem = "x86_64-linux";
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+      ];
 
-    pkgs = nixpkgs.legacyPackages.${clientSystem};
-  in {
-    colmena = {
-      meta = {
-        nixpkgs = pkgs;
-        specialArgs = {
-          inherit unstable;
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: {
+        # Use nixos-generators to bootstrap
+        packages.sd-image-zhuang = nixos-generators.nixosGenerate {
+          system = "aarch64-linux";
+          format = "sd-aarch64";
+          modules = [
+            ./config/zhuang/initial.nix
+          ];
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.colmena
+          ];
         };
       };
 
-      zhuang = {
-        deployment = {
-          # A fixed IP address is configured in the router
-          targetHost = "192.168.0.60";
-          targetPort = 2022;
-          targetUser = "root";
+      flake = {
+        colmena = {
+          meta = {
+            nixpkgs = nixpkgs.legacyPackages.x86_64-linux;
+            specialArgs = {
+              inherit unstable;
+            };
+          };
+
+          zhuang = {
+            deployment = {
+              # A fixed IP address is configured in the router
+              targetHost = "192.168.0.60";
+              targetPort = 2022;
+              targetUser = "root";
+            };
+            nixpkgs.system = "aarch64-linux";
+            imports = [
+              <nixpkgs/nixos/modules/installer/sd-card/sd-image-aarch64.nix>
+              ./config/zhuang/initial.nix
+              ./config/zhuang/rest.nix
+            ];
+          };
         };
-        nixpkgs.system = "aarch64-linux";
-        imports = [
-          <nixpkgs/nixos/modules/installer/sd-card/sd-image-aarch64.nix>
-          ./config/zhuang/initial.nix
-          ./config/zhuang/rest.nix
-        ];
       };
     };
-
-    packages.${clientSystem} = {
-      # Use nixos-generators to bootstrap
-      sd-image-zhuang = nixos-generators.nixosGenerate {
-        system = "aarch64-linux";
-        format = "sd-aarch64";
-        modules = [
-          ./config/zhuang/initial.nix
-        ];
-      };
-    };
-
-    devShells.${clientSystem} = {
-      default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.colmena
-        ];
-      };
-    };
-  };
 }
