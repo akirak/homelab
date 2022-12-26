2e50de409946ac65edcf5d9737c80a189ac626d4
Akira Komamura <akira.komamura@gmail.com>
2022-12-26 21:55:40 +0900

    Add basic configuration for Hetzner Cloud

---
+++ config/profiles/hcloud.nix
@@ -0,0 +1,22 @@
+{modulesPath, ...}: {
+  imports = [
+    (modulesPath + "/profiles/qemu-guest.nix")
+    # (modulesPath + "/profiles/hardened.nix")
+    # (modulesPath + "/profiles/headless.nix")
+  ];
+
+  boot.initrd.availableKernelModules = ["ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi"];
+  boot.initrd.kernelModules = ["nvme"];
+
+  zramSwap.enable = true;
+  boot.cleanTmpDir = true;
+
+  fileSystems."/" = {
+    device = "/dev/sda1";
+    fsType = "ext4";
+  };
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "/dev/sda";
+}
--- flake.nix
+++ flake.nix
@@ -52,6 +52,56 @@
       };
 
       flake = {
+        nixosConfigurations = {
+          hcloud-basic = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+              ./config/profiles/hcloud.nix
+              {
+                networking.hostName = "default";
+                # networking.firewall.enable = true;
+                services.tailscale.enable = true;
+                networking.firewall.trustedInterfaces = ["tailscale0"];
+                nix.allowedUsers = ["root"];
+
+                users.users.root = {
+                  hashedPassword = "$y$j9T$/p0sG9urgKkB6o.1IYunU/$iueghwqXj9mxDPP9fVu36NeNvvVdf.tdSchG2ZK5WT1";
+                  # Allow root login only with my PGP card
+                  openssh.authorizedKeys.keys = [
+                    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDEHKzdRvr0KjzLNGVV7eNcjh0m8liuXR2JLj2UA0Qa0yep3yZuVEc/I3l57z4FF27YvFVgxhLAAzXupeI98l3QTYXfaL4SF64/IZHElSC4pH5hHNNDMF37DCVLBAeAxesSkqhVoUMsG8lDiLSHy24GQBt9mKxFk461eViyVxLnPwzs7NsDo2sKVLFkPIG+SFI9wFrvRZK30l/twgljNefSoJc5xlIr6XXme3rKp00T4DMPb2sC2a9yYG5SgihQuB1RJkPXrp1gvp0wD1vc+lmniGiJEWbSefq3Ntaue48+o+yMgnazCQXSc/ozxmoK2ZISztEW+CBk5V9uD9TU8w5V cardno:11 482 161"
+                  ];
+                };
+                time.timeZone = "Asia/Tokyo";
+                system.stateVersion = "22.11";
+
+                networking.firewall.allowedTCPPorts = [
+                  80
+                  443
+                ];
+
+                services.nginx = {
+                  enable = true;
+                };
+
+                # https://xeiaso.net/blog/paranoid-nixos-2021-07-18
+                services.openssh = {
+                  enable = true;
+                  passwordAuthentication = false;
+                  allowSFTP = false;
+                  challengeResponseAuthentication = false;
+                  extraConfig = ''
+                    AllowTcpForwarding yes
+                    X11Forwarding no
+                    AllowAgentForwarding no
+                    AllowStreamLocalForwarding no
+                    AuthenticationMethods publickey
+                  '';
+                };
+              }
+            ];
+          };
+        };
+
         colmena = {
           meta = {
             nixpkgs = nixpkgs.legacyPackages.x86_64-linux;
