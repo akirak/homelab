f316f874bd2d9c4dc380b3b0bdb15b9278a427a5
Akira Komamura <akira.komamura@gmail.com>
2023-01-15 17:23:10 +0900

    machines/shu: Set the time zone to PST

--- flake.nix
+++ flake.nix
@@ -79,7 +79,7 @@
                     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDEHKzdRvr0KjzLNGVV7eNcjh0m8liuXR2JLj2UA0Qa0yep3yZuVEc/I3l57z4FF27YvFVgxhLAAzXupeI98l3QTYXfaL4SF64/IZHElSC4pH5hHNNDMF37DCVLBAeAxesSkqhVoUMsG8lDiLSHy24GQBt9mKxFk461eViyVxLnPwzs7NsDo2sKVLFkPIG+SFI9wFrvRZK30l/twgljNefSoJc5xlIr6XXme3rKp00T4DMPb2sC2a9yYG5SgihQuB1RJkPXrp1gvp0wD1vc+lmniGiJEWbSefq3Ntaue48+o+yMgnazCQXSc/ozxmoK2ZISztEW+CBk5V9uD9TU8w5V cardno:11 482 161"
                   ];
                 };
-                time.timeZone = "Asia/Tokyo";
+                time.timeZone = "America/Los_Angeles";
                 system.stateVersion = "22.11";
 
                 networking.firewall.allowedTCPPorts = [
