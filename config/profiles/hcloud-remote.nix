eb4fe1865a66278c73042fe2bd52ec58c6049d73
Akira Komamura <akira.komamura@gmail.com>
2023-01-15 14:52:39 +0900

    Try activating the previous options

--- config/profiles/hcloud-remote.nix
+++ config/profiles/hcloud-remote.nix
@@ -2,21 +2,22 @@
 # See https://github.com/numtide/nixos-remote
 {modulesPath, ...}: {
   imports = [
-    # (modulesPath + "/installer/scan/not-detected.nix")
+    (modulesPath + "/installer/scan/not-detected.nix")
     (modulesPath + "/profiles/qemu-guest.nix")
-    # (modulesPath + "/profiles/hardened.nix")
-    # (modulesPath + "/profiles/headless.nix")
+    (modulesPath + "/profiles/hardened.nix")
+    (modulesPath + "/profiles/headless.nix")
   ];
 
-  # boot.initrd.availableKernelModules = ["ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi"];
-  # boot.initrd.kernelModules = ["nvme"];
+  boot.initrd.availableKernelModules = [
+    "ata_piix"
+    "uhci_hcd"
+    "xen_blkfront"
+    "vmw_pvscsi"
+  ];
+  boot.initrd.kernelModules = ["nvme"];
 
-  # zramSwap.enable = true;
-  # boot.cleanTmpDir = true;
-
-  # boot.loader.grub.enable = true;
-  # boot.loader.grub.version = 2;
-  # boot.loader.grub.device = "/dev/sda";
+  zramSwap.enable = true;
+  boot.cleanTmpDir = true;
 
   boot.loader.grub = {
     devices = [
