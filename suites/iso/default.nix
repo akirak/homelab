6f9b94e8876ce002bc8e411dbf9607b968a60fc2
Akira Komamura <akira.komamura@gmail.com>
2023-01-20 20:20:25 +0900

    Add ISO image for ASUS BR1100

---
+++ suites/iso/default.nix
@@ -0,0 +1,13 @@
+{lib, pkgs, config, modulesPath, ...}:
+{
+  imports = [
+    ../base
+    (modulesPath + "/installer/cd-dvd/channel.nix")
+    (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix")
+  ];
+
+  # Faster compression algorithm. See https://nixos.wiki/wiki/Creating_a_NixOS_live_CD
+  isoImage.squashfsCompression = "gzip -Xcompression-level 1";
+
+  system.stateVersion = lib.mkDefault lib.trivial.release;
+}
