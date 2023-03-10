5cf907e96f1d818124314df5d18dd68782979f23
Akira Komamura <akira.komamura@gmail.com>
2023-03-10 13:40:01 +0900

    profiles/nix: Add substituters for the root

--- profiles/nix/default.nix
+++ profiles/nix/default.nix
@@ -15,6 +15,23 @@ If you don't build Nix on the host, don't import this profile.
       # sandbox = true;
       allowed-users = ["@wheel"];
       trusted-users = ["root" "@wheel"];
+
+      substituters = [
+        "https://cache.nixos.org/"
+        "https://akirak.cachix.org"
+      ];
+
+      trusted-substituters = [
+        "https://nix-community.cachix.org"
+        "https://emacs-ci.cachix.org"
+      ];
+
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "akirak.cachix.org-1:WJrEMdV1dYyALkOdp/kAECVZ6nAODY5URN05ITFHC+M="
+        "emacs-ci.cachix.org-1:B5FVOrxhXXrOL0S+tQ7USrhjMT5iOPH+QN9q0NItom4="
+      ];
     };
 
     extraOptions = ''
