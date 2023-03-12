3bc556e15699c950c71be4dfee13d8d3b8b2e6cc
Akira Komamura <akira.komamura@gmail.com>
2023-03-12 20:01:30 +0900

    templates/home-manager: Remove common options

--- templates/home-manager/home.nix
+++ templates/home-manager/home.nix
@@ -9,18 +9,9 @@
 
   programs.emacs-twist = {
     enable = true;
-    emacsclient.enable = true;
-    directory = ".local/share/emacs";
-    earlyInitFile = emacs-config.outPath + "/emacs/early-init.el";
-    createInitFile = true;
-    config = emacs-config.packages.${pkgs.system}.emacs-config.override {
+    settings = {
       extraFeatures = [
       ];
-      prependToInitFile = ''
-        ;; -*- lexical-binding: t; no-byte-compile: t; -*-
-        (setq custom-file (locate-user-emacs-file "custom.el"))
-        (setq akirak/enabled-status-tags t)
-      '';
     };
   };
 }
