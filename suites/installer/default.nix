ed8e688988c7bd0ae4e617f20eb6ba9bc5d8c9d9
Akira Komamura <akira.komamura@gmail.com>
2023-03-22 16:28:36 +0900

    suites/installer: Init

---
+++ suites/installer/default.nix
@@ -0,0 +1,41 @@
+{pkgs, ...}: {
+  imports =[
+    ../base
+    ../../profiles/nix
+  ];
+
+  networking.networkmanager.enable = true;
+
+  environment.systemPackages = [
+    pkgs.git
+    # Provided from disko flake via overlayModule
+    pkgs.disko
+
+    # Import useful packages from the package list in
+    # nixos/modules/profiles/base.nix of nixpkgs.
+    pkgs.gptfdisk
+    pkgs.efibootmgr
+    pkgs.efivar
+    pkgs.parted
+    pkgs.gptfdisk
+    pkgs.ddrescue
+    pkgs.ccrypt
+    pkgs.cryptsetup
+    pkgs.mkpasswd
+
+    pkgs.fuse
+    pkgs.fuse3
+    pkgs.rsync
+    pkgs.socat
+
+    pkgs.ntfsprogs
+    pkgs.dosfstools
+    pkgs.mtools
+    pkgs.xfsprogs.bin
+    pkgs.jfsutils
+    pkgs.f2fs-tools
+
+    pkgs.unzip
+    pkgs.zip
+  ];
+}
