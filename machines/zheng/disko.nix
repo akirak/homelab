464ad42d74452afcb0c217bd9c5265469a4a09cc
Akira Komamura <akira.komamura@gmail.com>
2023-04-29 06:05:00 +0900

    machines/zheng/disko: Update the disko config

--- machines/zheng/disko.nix
+++ machines/zheng/disko.nix
@@ -3,58 +3,60 @@
   disks ? [
     "/dev/disk/by-id/usb-Corsair_Voyager_GTX_511190321261A0D60061-0:0"
   ],
-  luksKey ? "/etc/luks-cryptroot.key",
+  luksKey ? "/etc/luks-corsair1.key",
   ...
 }: {
-  disk.mmcblk0 = {
-    device = builtins.elemAt disks 0;
-    type = "disk";
-    content = {
-      type = "table";
-      format = "gpt";
-      partitions = [
-        {
-          name = "ESP";
-          start = "0";
-          end = "500MiB";
-          bootable = true;
-          content = {
-            type = "filesystem";
-            format = "vfat";
-            mountpoint = "/boot";
-            mountOptions = [
-              "defaults"
-            ];
-          };
-        }
-
-        {
-          name = "luks";
-          start = "500MiB";
-          end = "100%";
-          content = {
-            type = "luks";
-            name = "cryptroot";
-            keyFile = luksKey;
+  disko.devices = {
+    disk.sda1 = {
+      device = builtins.elemAt disks 0;
+      type = "disk";
+      content = {
+        type = "table";
+        format = "gpt";
+        partitions = [
+          {
+            name = "ESP";
+            start = "0";
+            end = "500MiB";
+            bootable = true;
             content = {
-              type = "btrfs";
-              extraArgs = ["-f"];
-              subvolumes = {
-                "/root" = {
-                  mountpoint = "/";
-                  mountOptions = ["discard=async"];
-                };
-                "/nix" = {
-                  mountOptions = ["compress=zstd" "noatime" "discard=async"];
-                };
-                "/home" = {
-                  mountOptions = ["compress=zstd" "discard=async"];
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [
+                "defaults"
+              ];
+            };
+          }
+
+          {
+            name = "luks";
+            start = "500MiB";
+            end = "100%";
+            content = {
+              type = "luks";
+              name = "cryptcorsair1";
+              keyFile = luksKey;
+              content = {
+                type = "btrfs";
+                extraArgs = ["-f"];
+                subvolumes = {
+                  "/root" = {
+                    mountpoint = "/";
+                    mountOptions = ["discard=async"];
+                  };
+                  "/nix" = {
+                    mountOptions = ["compress=zstd" "noatime" "discard=async"];
+                  };
+                  "/home" = {
+                    mountOptions = ["compress=zstd" "discard=async"];
+                  };
                 };
               };
             };
-          };
-        }
-      ];
+          }
+        ];
+      };
     };
   };
 }
