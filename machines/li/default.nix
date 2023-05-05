6946a45db15c9097fb9aeec06ca22d867519859c
Akira Komamura <akira.komamura@gmail.com>
2023-05-06 08:35:03 +0900

    machines/{hui,li}: Explicitly set the name of the default user
    I won't set the default value of the user here because I need to use different
    identities depending on the context. If there is no user name set, it will
    default to none.

--- machines/li/default.nix
+++ machines/li/default.nix
@@ -61,6 +61,7 @@ in {
   };
 
   users.users.${homeUser} = {
+    name = "Akira Komamura";
     uid = 1000;
     isNormalUser = true;
     hashedPassword = "$6$3LmgpFGu4WEeoTss$9NQpF4CEO8ivu0uJTlDYXdiB6ZPHBsLXDZr.6S59bBNxmNuhirmcOmHTwhccdgSwq7sJOz2JbOOzmOCivxdak0";
