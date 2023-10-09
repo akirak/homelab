eba9b1ba4c2853ecd1a4f715865ccb35d079c36c
Akira Komamura <akira.komamura@gmail.com>
2023-10-09 11:17:24 +0900

    machines/li: Switch to rootless docker

--- machines/li/default.nix
+++ machines/li/default.nix
@@ -31,8 +31,8 @@ in {
     # ../../profiles/wayland/wm/river.nix
     ../../profiles/nix/cachix-deploy.nix
     ../../profiles/postgresql/development.nix
-    # ../../profiles/docker/rootless.nix
-    ../../profiles/docker
+    ../../profiles/docker/rootless.nix
+    # ../../profiles/docker
     # ../../profiles/k3s/single-node.nix
   ];
 
