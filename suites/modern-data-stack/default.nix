da1929d79b8f801ef32600a0ccc1ee14c97a3c4e
Akira Komamura <akira.komamura@gmail.com>
2024-01-02 14:09:24 +0900

    suites/modern-data-stack: Put containers inside a dedicated pod

--- suites/modern-data-stack/default.nix
+++ suites/modern-data-stack/default.nix
@@ -1,25 +1,68 @@
+# Based on https://github.com/weissi1994/nix/blob/938a0098120128bf9b5e89a7e62ebbae371bb993/hosts/_mixins/server/roles/immich.nix
 let
-  inherit (builtins) toString;
-in let
-  backend = "podman";
+  inherit (builtins) toString map attrNames;
+in
+  {
+    lib,
+    pkgs,
+    ...
+  }: let
+    backend = "podman";
 
-  kestraPort = 9900;
+    podName = "datastack";
 
-  containers = {
-    kestra = {
-      image = "kestra/kestra:latest-full";
-      user = "root";
-      ports = [
-        "127.0.0.1:${toString kestraPort}:8080"
-      ];
-      cmd = [
-        "server"
-        "local"
-      ];
+    kestraPort = 9900;
+
+    mkContainer = name: attrs:
+      lib.nameValuePair "${podName}-${name}"
+      (attrs
+        // {
+          autoStart = true;
+          extraOptions =
+            [
+              "--pod"
+              podName
+            ]
+            ++ (attrs.extraOptions or []);
+        });
+
+    containers = lib.mapAttrs' mkContainer {
+      kestra = {
+        image = "kestra/kestra:latest-full";
+        # Needed iff the container is not run as root
+        # user = "root";
+        ports = [
+          "${toString kestraPort}:8080"
+        ];
+        cmd = [
+          "server"
+          "local"
+        ];
+      };
     };
-  };
-in {
-  virtualisation.oci-containers = {
-    inherit backend containers;
-  };
-}
+
+    containerUnits = map (
+      name: "${backend}-${name}.service"
+    ) (attrNames containers);
+
+    createPodService = "create-${podName}-pod";
+  in {
+    systemd.services.${createPodService} = {
+      serviceConfig = {
+        Type = "oneshot";
+        Restart = "no";
+      };
+      # Don't share the net namespace and explicitly specify published ports.
+      script = ''
+        ${pkgs.podman}/bin/podman pod create --replace \
+           --share ipc,uts --publish ${toString kestraPort} \
+           --name ${podName}
+      '';
+      wantedBy = containerUnits;
+      before = containerUnits;
+    };
+
+    virtualisation.oci-containers = {
+      inherit backend containers;
+    };
+  }
