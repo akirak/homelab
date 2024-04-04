579307ddc10f0047a54b536ddb667a02964f386f
Akira Komamura <akira.komamura@gmail.com>
2024-04-04 18:33:15 +0900

    machines/li: Disable VirtualBox

--- machines/li/default.nix
+++ machines/li/default.nix
@@ -34,7 +34,7 @@ in {
     # ../../profiles/nix/cachix-deploy.nix
     ../../profiles/postgresql/development.nix
     ../../profiles/minio/development.nix
-    ../../profiles/virtualbox-host
+    # ../../profiles/virtualbox-host
     # ../../profiles/docker/rootless.nix
     ../../profiles/docker
     ../../profiles/docker/kind.nix
