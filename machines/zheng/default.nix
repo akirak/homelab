230abefb605e392d2e23760b948a4a6cd0b0edb7
Akira Komamura <akira.komamura@gmail.com>
2024-05-06 17:58:07 +0900

    machines/zheng: Fine-tune the NixOS configuration

--- machines/zheng/default.nix
+++ machines/zheng/default.nix
@@ -7,12 +7,11 @@
 in {
   imports = [
     # (modulesPath + "/profiles/headless.nix")
-    # ../../suites/server
     # ../../profiles/tailscale
     # ../../profiles/nginx
     # ../../profiles/nix/cachix-deploy.nix
+    ../../profiles/openssh
     # ./boot.nix
-    # (import ./disko.nix {})
     ./router.nix
   ];
 
@@ -58,28 +57,18 @@ in {
 
   time.timeZone = "Asia/Tokyo";
 
-  # nix.settings.allowed-users = ["root"];
+  nix.settings.allowed-users = ["root"];
 
-  # services.journald.extraConfig = ''
-  #   SystemMaxUse=1G
-  #   MaxFileSec=10day
-  # '';
+  services.journald.extraConfig = ''
+    SystemMaxUse=1G
+    MaxFileSec=10day
+  '';
 
-  # boot.tmp.cleanOnBoot = true;
+  boot.tmp.cleanOnBoot = true;
 
-  powerManagement.cpuFreqGovernor = "schedutil";
+  powerManagement.cpuFreqGovernor = "ondemand";
 
-  # zramSwap.enable = true;
-
-  users.users.akirakomamura = {
-    uid = 1000;
-    isNormalUser = true;
-    hashedPassword = "$y$j9T$6LW46s8StpmW2y3zzZ.qk0$ze1ABRCpZAPJ6Vp8LpTje8k5sH81P2HyyARByG598DB";
-
-    extraGroups = [
-      "wheel"
-    ];
-  };
+  zramSwap.enable = true;
 
   sdImage.compressImage = false;
 }
