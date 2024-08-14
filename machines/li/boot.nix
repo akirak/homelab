f9cf8798ccb4ed84c6ec3fe47c9e3812d59ba150
Akira Komamura <akira.komamura@gmail.com>
2024-08-14 19:32:58 +0900

    Apply nixfmt

--- machines/li/boot.nix
+++ machines/li/boot.nix
@@ -3,29 +3,38 @@
   pkgs,
   lib,
   ...
-}: let
+}:
+let
   annex-dm = "local_annex";
-in {
+in
+{
   boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
 
-  boot.binfmt.emulatedSystems = ["aarch64-linux"];
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod"];
+  boot.initrd.availableKernelModules = [
+    "xhci_pci"
+    "ahci"
+    "nvme"
+    "usbhid"
+    "usb_storage"
+    "sd_mod"
+  ];
   # This kernel module is needed if and only if unlock LUKS devices on boot
   # boot.initrd.kernelModules = [ "dm-snapshot" ];
-  boot.kernelModules = ["kvm-intel"];
-  boot.extraModulePackages = [];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
 
   # ZFS support
-  boot.supportedFilesystems = ["zfs"];
-  boot.initrd.supportedFilesystems = ["zfs"];
+  boot.supportedFilesystems = [ "zfs" ];
+  boot.initrd.supportedFilesystems = [ "zfs" ];
   boot.zfs.requestEncryptionCredentials = true;
   services.zfs.autoSnapshot.enable = true;
   services.zfs.autoScrub.enable = true;
   # Configure ARC up to 4 GiB
-  boot.kernelParams = ["zfs.zfs_arc_max=4294967296"];
+  boot.kernelParams = [ "zfs.zfs_arc_max=4294967296" ];
 
   environment.systemPackages = [
     pkgs.lshw
@@ -35,13 +44,20 @@ in {
   fileSystems."/" = {
     device = "tmpfs";
     fsType = "tmpfs";
-    options = ["defaults" "size=10G" "mode=755"];
+    options = [
+      "defaults"
+      "size=10G"
+      "mode=755"
+    ];
   };
 
   fileSystems."/boot" = {
     device = "/dev/disk/by-uuid/B327-FDEA";
     fsType = "vfat";
-    options = ["fmask=0137" "dmask=0027"];
+    options = [
+      "fmask=0137"
+      "dmask=0027"
+    ];
   };
 
   fileSystems."/nix" = {
@@ -57,15 +73,18 @@ in {
   fileSystems."/git-annex/${config.networking.hostName}" = {
     device = "/dev/mapper/${annex-dm}";
     fsType = "ext4";
-    options = ["relatime" "discard"];
+    options = [
+      "relatime"
+      "discard"
+    ];
   };
 
-  swapDevices = [];
+  swapDevices = [ ];
 
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
   nixpkgs.config.packageOverrides = pkgs: {
-    vaapiIntel = pkgs.vaapiIntel.override {enableHybridCodec = true;};
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
   };
 
   hardware.graphics = {
