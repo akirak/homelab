9bf835f0657cb670915ab886fd15df1c8d3cf01d
Akira Komamura <akira.komamura@gmail.com>
2024-11-30 12:33:13 +0900

    chore(modules): Update r8168

--- modules/models/asus-br1100/r8168.nix
+++ modules/models/asus-br1100/r8168.nix
@@ -2,28 +2,26 @@
   pkgs,
   config,
   ...
-}: {
+}:
+{
   # At present, wired interface does not work even with this kernel module due
   # to a common "ucsi_acpi usbc000:00: ppm init failed" error. This issue may be
   # fixed at some point, but I am not sure.
   boot.extraModulePackages = [
-    (config.boot.kernelPackages.r8168.overrideAttrs (_: super: rec {
-      version = "8.051.02";
-      src = pkgs.fetchFromGitHub {
-        owner = "mtorromeo";
-        repo = "r8168";
-        rev = version;
-        sha256 = "sha256-osANis1wDOXp3eShNMUA8IlIvcHjksdt+3V1cy3It5o=";
-      };
-      patches = [
-        ./r8168.patch
-      ];
-      meta =
-        super.meta
-        // {
+    (config.boot.kernelPackages.r8168.overrideAttrs (
+      _: super: rec {
+        version = "8.054.00";
+        src = pkgs.fetchFromGitHub {
+          owner = "mtorromeo";
+          repo = "r8168";
+          rev = version;
+          sha256 = "sha256-KyycAe+NBmyDDH/XkAM4PpGvXI5J1CuMW4VuHcOm0UQ=";
+        };
+        meta = super.meta // {
           broken = false;
         };
-    }))
+      }
+    ))
   ];
-  boot.blacklistedKernelModules = ["r8169"];
+  boot.blacklistedKernelModules = [ "r8169" ];
 }
