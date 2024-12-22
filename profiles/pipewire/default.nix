3a4a4c2d472abeeb14cf22cb5daf64b5c96f7098
Akira Komamura <akira.komamura@gmail.com>
2024-12-22 13:35:16 +0900

    profiles/pipewire: Enable ALSA

--- profiles/pipewire/default.nix
+++ profiles/pipewire/default.nix
@@ -8,8 +8,8 @@
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
-    # alsa.enable = true;
-    # alsa.support32Bit = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
