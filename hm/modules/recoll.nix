d7a4d8efedf5c6cee9287eb5ee4aefd33c09fc44
Akira Komamura <akira.komamura@gmail.com>
2025-01-20 23:42:59 +0900

    hm/modules/recoll: Add directories

--- hm/modules/recoll.nix
+++ hm/modules/recoll.nix
@@ -8,6 +8,8 @@
       "~/resources/ebooks"
       "~/resources/sound"
       "~/resources/bundles"
+      "~/notes-and-pdfs/notes"
+      "~/notes-and-pdfs/ebooks"
     ];
 
     excludedmimetypes = [
