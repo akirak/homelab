9a5251fe53801dd7d1a11fcc5130e35dcb2fd7ba
Akira Komamura <akira.komamura@gmail.com>
2025-03-26 00:09:54 +0900

    flake: Allow overriding the IP address of the deployed host

--- flake.nix
+++ flake.nix
@@ -174,19 +174,42 @@
                 runtimeInputs = [ stable.legacyPackages.${system}.nixos-rebuild ];
                 meta.description = "A nixos-rebuild wrapper that targets a host on LAN";
                 text = ''
-                  if [[ $# -eq 0 ]]; then
+                  target_host="${hostName}"
+
+                  usage() {
+                    echo "deploy [--target-host IP] switch|test ARGS"
+                  }
+
+                  while [[ $# -gt 0 ]]; do
+                    case "$1" in
+                      --help|-h)
+                        usage
+                        exit
+                        ;;
+                      --target-host)
+                        target_host="$2"
+                        shift; shift
+                        ;;
+                      *)
+                        mode="$1"
+                        shift
+                        break
+                        ;;
+                    esac
+                  done
+
+                  if ! [[ -v mode ]]; then
                     echo >&2 "You need to specify one of the subcommands of nixos-rebuild"
                     exit 1
                   fi
 
-                  mode="$1"
-                  shift
+                  set -x
 
                   # Don't look up known_hosts file because the host key is updated on every deploy
                   NIX_SSHOPTS="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" \
                     nixos-rebuild "$mode" \
                     --flake ".#${hostName}" \
-                    --target-host "root@${hostName}" \
+                    --target-host "root@''${target_host}" \
                     --option accept-flake-config true \
                     "$@"
                 '';
