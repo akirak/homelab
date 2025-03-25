1357bf6f80c46d44283042c7ee347db86d9520fa
Akira Komamura <akira.komamura@gmail.com>
2025-03-26 02:07:41 +0900

    hm/code: Add npx and uvx

--- hm/code.nix
+++ hm/code.nix
@@ -1,4 +1,24 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
+let
+  # Build an interpreter-only derivation for running a package directly.
+  onlySingleBin =
+    drv: name:
+    (pkgs.callPackage (
+      { runCommand, makeWrapper }:
+      runCommand name
+        {
+          buildInputs = [
+            makeWrapper
+          ];
+          propagatedBuildInputs = [ drv ];
+        }
+        ''
+          mkdir -p $out/bin
+          makeWrapper ${lib.getBin drv}/bin/${name} $out/bin/${name} \
+            --prefix PATH : ${lib.getBin drv}/bin
+        ''
+    ) { });
+in
 {
   home.packages = with pkgs; [
     yamlfmt
@@ -7,5 +27,9 @@
 
     # AI frontends
     claude-code
+
+    # Used to run MCP servers.
+    (onlySingleBin pkgs.nodejs "npx")
+    (onlySingleBin pkgs.uv "uvx")
   ];
 }
