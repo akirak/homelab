4d19ef05f4568ab669e19ae4f47a509b6059c1c0
Akira Komamura <akira.komamura@gmail.com>
2025-05-03 15:29:27 +0900

    Remove the override on anytree

--- flake.nix
+++ flake.nix
@@ -104,15 +104,6 @@
               sha256 = "sha256-Hb89cqx744pDxm//Lm3di9cIIFoBVyownXjY5QAk3lc=";
             };
           };
-          # TODO wait for https://github.com/c0fec0de/anytree/issues/270
-          # TODO wait for https://github.com/NixOS/nixpkgs/issues/375763
-          python3Packages = prev.python3Packages.overrideScope (
-            _finalPy: prevPy: {
-              anytree = prevPy.anytree.overrideAttrs (old: {
-                patches = old.patches ++ [ ./patches/python-anytree-poetry-project-name-version.patch ];
-              });
-            }
-          );
           intel-media-driver = prev.intel-media-driver.overrideAttrs {
             version = "24.4.4";
             # Use the master newer than 24.4.4 for bugfixes.
